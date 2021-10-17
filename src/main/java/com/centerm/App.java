3946d307ac59a9163001edaf0d69efd4988da147
骑公路车的小菜鸟 <1347401764@qq.com>
2021-10-17 15:49:52 +0800

    first commit

---
+++ src/main/java/com/centerm/App.java
@@ -0,0 +1,13 @@
+package com.centerm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
